24487cbd2b3ee8bff765eb0811f193b227294dbd
HYOJU KANG <71005886+kang295@users.noreply.github.com>
2023-03-18 14:45:34 -0400

    commit

---
+++ Medium/626. Exchange Seats.sql
@@ -0,0 +1,60 @@
+/*
+Table: Seat
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| student     | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the name and the ID of a student.
+id is a continuous increment.
+ 
+
+Write an SQL query to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+
+Return the result table ordered by id in ascending order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Seat table:
++----+---------+
+| id | student |
++----+---------+
+| 1  | Abbot   |
+| 2  | Doris   |
+| 3  | Emerson |
+| 4  | Green   |
+| 5  | Jeames  |
++----+---------+
+Output: 
++----+---------+
+| id | student |
++----+---------+
+| 1  | Doris   |
+| 2  | Abbot   |
+| 3  | Green   |
+| 4  | Emerson |
+| 5  | Jeames  |
++----+---------+
+Explanation: 
+Note that if the number of students is odd, there is no need to change the last one's seat.
+*/
+
+-- solution:
+select 
+(case
+    when mod(id, 2) != 0 and counts != id then id + 1
+    when mod(id, 2) != 0 and counts = id then id
+    else id - 1
+    end) as id, student
+from seat,
+(select count(*) as counts
+from seat) as seat_counts
+order by id ASC
