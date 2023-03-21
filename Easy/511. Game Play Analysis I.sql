7cb133f7641a9bb0bc80372ca7e66a7713d2cc13
HYOJU KANG <71005886+kang295@users.noreply.github.com>
2023-03-21 17:55:22 -0400

    update

--- Easy/511. Game Play Analysis I.sql
+++ Easy/511. Game Play Analysis I.sql
@@ -29,9 +29,7 @@ Activity table:
 +-----------+-----------+------------+--------------+
 | player_id | device_id | event_date | games_played |
 +-----------+-----------+------------+--------------+
-| 1         | 2         | 2016-03-01 | 5            |select player_id, min(event_date) AS first_login
-from Activity
-group by player_id
+| 1         | 2         | 2016-03-01 | 5            |
 | 1         | 2         | 2016-05-02 | 6            |
 | 2         | 3         | 2017-06-25 | 1            |
 | 3         | 1         | 2016-03-02 | 0            |
