7a0e297cfa0140c18d6952641033527a60d30ce0
HYOJU KANG <71005886+kang295@users.noreply.github.com>
2023-03-25 14:54:35 -0400

    update

--- Easy/197. Rising Temperature.sql
+++ Easy/197. Rising Temperature.sql
@@ -44,7 +44,13 @@ In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
 In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
 */
 
--- solution:
+-- solution 1:
 select w2.Id
 from weather w1, weather w2
 where w2.recordDate = Date_add(w1.recordDate, Interval 1 day) and w2.Temperature > w1.Temperature
+
+-- solution 2:
+select w1.id
+from weather w1, weather w2
+where w1.temperature > w2.temperature and
+    to_days(w1.recorddate) - to_days(w2.recorddate) = 1
