56932f5b0cef8bd9afcbe80235043e6ed5b5d773
HYOJU KANG <71005886+kang295@users.noreply.github.com>
2023-04-02 15:38:41 -0400

    commit

---
+++ Medium/570. Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,56 @@
+/*
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+ 
+
+Write an SQL query to report the managers with at least five direct reports.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | None      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+
+*/
+
+-- solution:
+SELECT name
+FROM Employee AS t1 
+JOIN
+    (SELECT ManagerId
+    FROM Employee
+    GROUP BY ManagerId
+    HAVING COUNT(ManagerId) >= 5) AS t2
+ON t1.Id = t2.ManagerId
