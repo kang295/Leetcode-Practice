cd123805bfd27e9d5ad8d41a99425be7d28c7299
Hyoju Kang <71005886+kang295@users.noreply.github.com>
2023-08-28 17:05:13 -0400

    commit

--- Medium/1174. Immediate Food Delivery II.sql
+++ Medium/1174. Immediate Food Delivery II.sql
@@ -1,3 +1,4 @@
+/*
 Table: Delivery
 
 +-----------------------------+---------+
@@ -49,6 +50,7 @@ The customer id 2 has a first order with delivery id 2 and it is immediate.
 The customer id 3 has a first order with delivery id 5 and it is scheduled.
 The customer id 4 has a first order with delivery id 7 and it is immediate.
 Hence, half the customers have immediate first orders.
+*/
 
 -- solution:
 select round(avg(order_date = customer_pref_delivery_date) * 100, 2) as immediate_percentage
