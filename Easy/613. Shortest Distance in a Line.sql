ffc667e4aebddb8b2a6fe59638356291383b6c69
Hyoju Kang <71005886+kang295@users.noreply.github.com>
2023-10-01 14:33:02 -0400

    commit

---
+++ Easy/613. Shortest Distance in a Line.sql
@@ -0,0 +1,45 @@
+/*
+Table: Point
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
++-------------+------+
+In SQL, x is the primary key column for this table.
+Each row of this table indicates the position of a point on the X-axis.
+ 
+
+Find the shortest distance between any two points from the Point table.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Point table:
++----+
+| x  |
++----+
+| -1 |
+| 0  |
+| 2  |
++----+
+Output: 
++----------+
+| shortest |
++----------+
+| 1        |
++----------+
+Explanation: The shortest distance is between points -1 and 0 which is |(-1) - 0| = 1.
+*/
+
+-- solution:
+select abs(a.x-b.x) as shortest
+from Point as a 
+cross join Point as b
+where a.x != b.x
+order by shortest ASC
+limit 1
