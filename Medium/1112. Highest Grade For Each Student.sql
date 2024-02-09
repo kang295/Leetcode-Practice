6873353724fd6958d84b9e2ad5a6defe2cac83dd
Hyoju Kang <71005886+kang295@users.noreply.github.com>
2024-02-09 16:39:39 -0500

    commit

---
+++ Medium/1112. Highest Grade For Each Student.sql
@@ -0,0 +1,55 @@
+/*
+Table: Enrollments
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| student_id    | int     |
+| course_id     | int     |
+| grade         | int     |
++---------------+---------+
+(student_id, course_id) is the primary key (combination of columns with unique values) of this table.
+grade is never NULL.
+ 
+
+Write a solution to find the highest grade with its corresponding course for each student. In case of a tie, you should find the course with the smallest course_id.
+
+Return the result table ordered by student_id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Enrollments table:
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 2          | 2         | 95    |
+| 2          | 3         | 95    |
+| 1          | 1         | 90    |
+| 1          | 2         | 99    |
+| 3          | 1         | 80    |
+| 3          | 2         | 75    |
+| 3          | 3         | 82    |
++------------+-----------+-------+
+Output: 
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 1          | 2         | 99    |
+| 2          | 2         | 95    |
+| 3          | 3         | 82    |
++------------+-----------+-------+
+*/
+
+-- solution:
+with cte as (SELECT *, 
+ROW_NUMBER() OVER(PARTITION BY student_id ORDER BY grade DESC, course_id) AS grader
+FROM Enrollments)
+
+select student_id, course_id, grade
+from cte
+where grader = 1
